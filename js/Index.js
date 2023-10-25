3ac5d9ebfb33b090dfebd76d98e93866de837562
Léhahiah Angels <lehahiahangels@gmail.com>
2023-10-25 06:07:52 +0100

    Dépot 002 +Modifiaction page tarifs et ajout page RDV

--- js/Index.js
+++ js/Index.js
@@ -1,6 +1,88 @@
 // Gestion du changement de style de la barre de navigation lors du défilement
-const navBar = document.querySelector(".nav-bar");
+document.addEventListener("DOMContentLoaded", function () {
+  const navBar = document.querySelector(".nav-bar");
 
-window.addEventListener("scroll", () => {
-  navBar.classList.toggle("scroll", window.scrollY > 0);
+  window.addEventListener("scroll", () => {
+    navBar.classList.toggle("scroll", window.scrollY > 0);
+  });
 });
+
+// Gestion du carrousel infini
+
+const wrapper = document.querySelector(".wrapper");
+const carousel = document.querySelector(".carousel");
+const firstCardWidth = carousel.querySelector(".card").offsetWidth;
+const arrowBtns = document.querySelectorAll(".wrapper i");
+const carouselChildrens = Array.from(carousel.children);
+
+let isDragging = false,
+  isAutoPlay = true,
+  startX,
+  startScrollLeft,
+  timeoutId;
+
+let cardPerView = Math.floor(carousel.offsetWidth / firstCardWidth);
+
+carouselChildrens.slice(-cardPerView).forEach((card) => {
+  carousel.insertAdjacentHTML("afterbegin", card.outerHTML);
+});
+
+carouselChildrens.slice(0, cardPerView).forEach((card) => {
+  carousel.insertAdjacentHTML("beforeend", card.outerHTML);
+});
+
+carousel.classList.add("no-transition");
+carousel.scrollLeft = carousel.offsetWidth;
+carousel.classList.remove("no-transition");
+
+arrowBtns.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    carousel.scrollLeft += btn.id === "left" ? -firstCardWidth : firstCardWidth;
+  });
+});
+
+const dragStart = (e) => {
+  isDragging = true;
+  carousel.classList.add("dragging");
+  startX = e.pageX;
+  startScrollLeft = carousel.scrollLeft;
+};
+
+const dragging = (e) => {
+  if (!isDragging) return;
+  carousel.scrollLeft = startScrollLeft - (e.pageX - startX);
+};
+
+const dragStop = () => {
+  isDragging = false;
+  carousel.classList.remove("dragging");
+};
+
+const infiniteScroll = () => {
+  if (
+    carousel.scrollLeft === 0 ||
+    Math.ceil(carousel.scrollLeft) ===
+      carousel.scrollWidth - carousel.offsetWidth
+  ) {
+    carousel.classList.add("no-transition");
+    carousel.scrollLeft = carousel.scrollWidth - 2 * carousel.offsetWidth;
+    carousel.classList.remove("no-transition");
+  }
+
+  clearTimeout(timeoutId);
+  if (!wrapper.matches(":hover")) autoPlay();
+};
+
+const autoPlay = () => {
+  if (window.innerWidth < 800 || !isAutoPlay) return;
+  timeoutId = setTimeout(() => (carousel.scrollLeft += firstCardWidth), 6500);
+};
+
+autoPlay();
+
+carousel.addEventListener("mousedown", dragStart);
+carousel.addEventListener("mousemove", dragging);
+document.addEventListener("mouseup", dragStop);
+carousel.addEventListener("scroll", infiniteScroll);
+wrapper.addEventListener("mouseenter", () => clearTimeout(timeoutId));
+wrapper.addEventListener("mouseleave", autoPlay);
